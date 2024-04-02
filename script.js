ef0e306185fedb67e1c324b7863ad2b5869f44b3
Noirkarl <carloscm4231@gmail.com>
2024-04-02 14:44:41 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,108 +1,38 @@
+let tiempoRef = Date.now()
+let cronometrar = false
+let acumulado = 0
 
-const botonNumero = document.querySelectorAll('[data-numero]')
-const botonOperador = document.querySelectorAll('[data-operador]')
-const botonIgual = document.querySelector('[data-igual]')
-const botonBorrarTodo = document.querySelector('[data-borrar-todo]')
-const botonBorrar = document.querySelector('[data-borrar]')
-const textoValorSuperior = document.querySelector('[data-valor-superior]')
-const textoValorInferior = document.querySelector('[data-valor-inferior]')
-
-class Calculadora {
-    constructor(textoValorInferior,textoValorSuperior){
-        this.textoValorInferior = textoValorInferior
-        this.textoValorSuperior = textoValorSuperior
-        this.valorInferior = ''
-        this.valorSuperior = ''
-        this.operador = undefined
+    function iniciar() {
+        cronometrar = true
     }
 
-    agregarNumero(numero){
-        if(numero === '.' && this.valorInferior.includes('.')) return
-        this.valorInferior = this.valorInferior + numero;
-    }
-    imprimirDisplay () {
-        this.textoValorInferior.innerText = this.valorInferior;
-        this.textoValorSuperior.innerText = this.valorSuperior;
-    }
-
-    borrar(){
-        this.valorInferior = this.valorInferior.slice(0,-1);
-    }
-
-    elegirOperacion(operador){
-        if(this.valorInferior == '') return
-        if(this.valorSuperior != ''){
-            this.realizarCalculo()
+        function pausar() {
+            cronometrar = false
         }
-        this.operador = operador;
-        this.valorSuperior = this.valorInferior;
-        this.valorInferior = '';
+
+    function reiniciar() {
+        acumulado = 0
     }
 
-    realizarCalculo(){
-        let resultado;
-        let conversionValorSuperior = parseFloat(this.valorSuperior);
-        let conversionValorInferior = parseFloat(this.valorInferior);
-        if(isNaN(conversionValorSuperior) || isNaN(conversionValorInferior)) return
-        switch (this.operador) {
-            case '+':
-            resultado = conversionValorSuperior + conversionValorInferior
-            break
-            case '-':
-            resultado = conversionValorSuperior - conversionValorInferior
-            break
-            case '*':
-            resultado = conversionValorSuperior * conversionValorInferior
-            break
-            case '÷':
-            resultado = conversionValorSuperior / conversionValorInferior
-            break
-            default: return
+    setInterval(() => {
+        let tiempo = document.getElementById("tiempo")
+        if (cronometrar) {
+            acumulado += Date.now() - tiempoRef
         }
-        this.valorInferior = resultado
-        this.operador = undefined
-        this.valorSuperior= ''
-    }
+        tiempoRef = Date.now()
+        tiempo.innerHTML = formatearMS(acumulado)
+    }, 1000 / 60);
 
-    limpiarPantalla(){
-        this.valorInferior = ''
-        this.valorSuperior = ''
-        this.operador = undefined
+    function formatearMS(tiempo_ms) {
+        let MS = tiempo_ms % 1000
+        let St = Math.floor(((tiempo_ms - MS) / 1000))
+        let S = St%60
+        let M = Math.floor((St / 60) % 60)
+        let H = Math.floor((St/60 / 60))
+        Number.prototype.ceros = function (n) {
+        return (this + "").padStart(n, 0)
+        }
 
-    }
+        return H.ceros(2) + ":" + M.ceros(2) + ":" + S.ceros(2)
+        + "." + MS.ceros(3)
 }
-
-
-const calculadora = new Calculadora(textoValorInferior,textoValorSuperior);
-
-
-
-
-botonNumero.forEach(boton => {
-    boton.addEventListener('click', () => {
-        calculadora.agregarNumero(boton.innerText)
-        calculadora.imprimirDisplay() 
-    })
-})
-
-botonBorrar.addEventListener('click', () => {
-    calculadora.borrar();
-    calculadora.imprimirDisplay();
-})
-
-botonOperador.forEach(boton => {
-    boton.addEventListener('click', () => {
-        calculadora.elegirOperacion(boton.innerText)
-        calculadora.imprimirDisplay() 
-    })
-})
-
-botonIgual.addEventListener('click', () => {
-    calculadora.realizarCalculo();
-    calculadora.imprimirDisplay();
-})
-
-botonBorrarTodo.addEventListener('click', () => {
-    calculadora.limpiarPantalla();
-    calculadora.imprimirDisplay();
-})
